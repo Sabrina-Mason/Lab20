804dcc39620fab1660f5fe184caa1da582a1a883
Sabrina Mason <fireflyy85@gmail.com>
2017-07-28 12:49:55 -0400

    update

---
+++ Lab20/Controllers/HomeController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+//using System.Data.Entity;
+using System.Web.Mvc;
+
+namespace Lab20.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "About Page";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact page";
+
+            return View();
+        }
+
+        public ActionResult UserRegistration()
+        {
+            ViewBag.Message = "User Registration here";
+            return View();
+        }
+        [Required]
+        public int Phone { get; set; }
+
+
+
+
+        public ActionResult Summary()
+        {
+
+            ViewBag.Message = "Your First Name";
+            return View();
+
+        }
+
+        public ActionResult Success(Register r)//register model passed into it, its an object//
+        {
+
+            ViewBag.Message = "Your email is " + r.Email;
+            return View(r);
+           
+        }
+
+
+
+
+
+
+
+        public class Register
+        {
+            [Required]
+            [Key]
+            public string FirstName { get; set; }
+            [Required]
+            public string Email { get; set; }
+        }
+
+
+
+
+    }
+
+}
